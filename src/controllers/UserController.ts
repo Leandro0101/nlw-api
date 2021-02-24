3262b099046ca1ab07a42834b25ce267ef319b68
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-24 20:51:38 -0300

    feat: create user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express'
+import { User } from '../models/User'
+import { getRepository } from 'typeorm'
 class UserController {
-  async create(request: Request, response: Response) {
-    const message: string = 'Hellow, controller is run'
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body
+    const userRepository = getRepository(User)    
+    const userAlreadyExists = await userRepository.findOne({ email})    
 
-    return response.send(message)
+    if(userAlreadyExists){
+      return response.status(400).json({ error: 'User already existis' })
+    }
+
+    const user = userRepository.create({
+      name, email
+    })
+
+    await userRepository.save(user)
+
+    return response.json(user)
   }
 }
 
