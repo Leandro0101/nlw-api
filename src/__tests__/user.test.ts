856da400576fbaf475db9a4b821fd458d074e99b
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-25 16:53:21 -0300

    test: Should be able to create a new user

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,9 +1,21 @@
 import { app } from '../app'
 import request from 'supertest'
+import createConnection from '../database'
 
 describe('Users', () => {
-  request(app).post('/users').send({
-    email: 'user@gmail.com',
-    name: 'user example'
+  
+  beforeAll(async () => {
+    const connection = await createConnection()
+    await connection.runMigrations()
+  })
+  
+  
+  test('Should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'user@gmail.com',
+      name: 'user example'
+    })
+
+    expect(response.status).toBe(201)
   })
 })
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -18,7 +18,7 @@ class UserController {
 
     await userRepository.save(user)
 
-    return response.json(user)
+    return response.status(201).json(user)
   }
 }
 
