5e176a382865d9c6f6adf6741371b42ad94b1610
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-25 15:11:29 -0300

    refactor: move data access responsability to a custom repository

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,7 @@
+import { User } from "../models/User";
+import { EntityRepository, Repository } from "typeorm";
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+
+}
