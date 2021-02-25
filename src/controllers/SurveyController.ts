79009f7589b67782586312ae8946d72f14f56928
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-25 16:11:27 -0300

    feat: create survey

---
+++ src/controllers/SurveyController.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from 'express'
+import { getCustomRepository } from 'typeorm';
+import { SurveysRepository } from '../repositories/SurveysRepository';
+class SurveyController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { title, description } = request.body
+    const surveysRepository = getCustomRepository(SurveysRepository)
+    const survey = surveysRepository.create({
+      title, description
+    })
+
+    await surveysRepository.save(survey)
+
+    return response.status(201).json(survey)
+  }
+}
+
+export const surveyController = new SurveyController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
+import { surveyController } from './controllers/SurveyController'
 import { userController } from './controllers/UserController'
 
 export const router = Router()
 
 router.post('/users', userController.create)
+router.post('/surveys', surveyController.create)
