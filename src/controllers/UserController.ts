e6dd13c1738887f76ebb4b025d90f855b3368821
Leandro0101 <leandrovieira3306@gmail.com>
2021-02-27 19:27:12 -0300

    feat: add validations

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
 import { UsersRepository } from '../repositories/UsersRepository'
-
+import * as yup from 'yup'
 class UserController {
   async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().required().email()
+    })
+
+    try {
+      await schema.validate(request.body)
+    } catch (error) {
+      return response.status(400).json({ error }) 
+    }
+    
     const userRepository = getCustomRepository(UsersRepository)    
     const userAlreadyExists = await userRepository.findOne({ email})    
 
